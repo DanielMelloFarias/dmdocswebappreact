3cd3e922afbddc535b5c9412f2fdf89fdc53daec
DanielMelloFarias <danielmellofarias@gmail.com>
2024-05-13 09:59:03 -0300

    add talkToDocument.js - adicionei rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // src/App.js
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import BlobUploader from "./components/BlobUploader";
+import TalkToDocuments from "./components/TalkToDocuments"; // Vamos criar esse componente depois
 
 function App() {
   return (
     <div>
       <h1>Azure Blob Storage File Uploader</h1>
-      <BlobUploader />
+      <Routes>
+        <Route path="/uploadFiles" element={<BlobUploader />} />
+        <Route path="/talkToDocuments" element={<TalkToDocuments />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/TalkToDocuments.js
@@ -0,0 +1,13 @@
+// src/components/TalkToDocuments.js
+import React from "react";
+
+const TalkToDocuments = () => {
+  return (
+    <div>
+      <h2>Talk to Documents</h2>
+      {/* Conteúdo do seu componente TalkToDocuments */}
+    </div>
+  );
+};
+
+export default TalkToDocuments;
